ff74d3068f254918fb854aeff0a0df9859b300db
li666 <libo02@vocalbeats.com>
2024-08-26 17:53:07 +0800

    feat:添加测试代码

---
+++ Test/test/src/views/TestView.vue
@@ -0,0 +1,37 @@
+
+<template>
+    <div class="about">
+      <h1>测试页面</h1>
+      <button  @click="test">button1</button>
+      <div>{{info}}</div>
+    </div>
+</template>
+  
+<script>
+  export default {
+    data() {
+      return {
+        info: "啊啊"
+      };
+    },
+    methods: {
+      test() {
+        console.log("测试");
+        this.info = window.Telegram.WebApp.version
+      },
+    },
+  };
+
+
+</script>
+
+  <style>
+  @media (min-width: 1024px) {
+    .about {
+      min-height: 100vh;
+      display: flex;
+      align-items: center;
+    }
+  }
+  </style>
+  
